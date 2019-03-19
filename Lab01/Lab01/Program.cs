70e3a123762ebfd2066cfbb2614d5f075309be9c
frank-harris <s00088225@acad.tri-c.edu>
2019-03-19 10:45:24 -0800

    Writeline to output text Readkey to make windows stay open until keypress

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,8 +7,8 @@
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Hello World from Frank Harris");
-        Console.ReadKey();
+            Console.WriteLine("Hello World from Frank Harris");
+            Console.ReadKey();
 
         }
     }
